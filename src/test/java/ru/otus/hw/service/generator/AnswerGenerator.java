1999b2b32dfc4a32d17d07d1a1b9ccf574ce10ac
d.losev <d.losev@infosec.ru>
2023-12-06 18:20:19 +0300

    complete

---
+++ src/test/java/ru/otus/hw/service/generator/AnswerGenerator.java
@@ -0,0 +1,29 @@
+package ru.otus.hw.service.generator;
+
+import ru.otus.hw.domain.Answer;
+
+import java.util.List;
+
+public class AnswerGenerator {
+
+	public static List<Answer> all() {
+		return List.of(
+				valid(),
+				emptyText()
+		);
+	}
+
+	public static Answer valid() {
+		return Answer.builder()
+				.text("valid answer")
+				.isCorrect(true)
+				.build();
+	}
+
+	public static Answer emptyText() {
+		return Answer.builder()
+				.text("")
+				.isCorrect(true)
+				.build();
+	}
+}
