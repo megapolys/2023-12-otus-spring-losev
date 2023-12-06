7cc7ab2950f56381ece4a280cdb60318a7283227
d.losev <d.losev@infosec.ru>
2023-12-06 18:07:42 +0300

    Initial commit

--- src/main/java/ru/otus/hw/domain/Answer.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.hw.domain;
-
-import lombok.Builder;
-
-@Builder
-public record Answer(String text, boolean isCorrect) {
-}
--- src/main/java/ru/otus/hw/domain/Question.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.hw.domain;
-
-import lombok.Builder;
-
-import java.util.List;
-
-@Builder
-public record Question(String text, List<Answer> answers) {
-}
