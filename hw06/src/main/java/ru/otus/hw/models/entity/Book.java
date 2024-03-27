1bc61d380f11d87649aa78e449edecaaf5364f78
d.losev <d.losev@infosec.ru>
2024-03-27 09:01:18 +0300

    hw09 complete

--- hw06/src/main/java/ru/otus/hw/models/entity/Book.java
+++ hw06/src/main/java/ru/otus/hw/models/entity/Book.java
@@ -1,23 +1,7 @@
 package ru.otus.hw.models.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.NamedAttributeNode;
-import jakarta.persistence.NamedEntityGraph;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -31,7 +15,8 @@ import java.util.List;
 @Entity
 @Table(name = "books")
 @NamedEntityGraph(name = "book-author-graph", attributeNodes = {@NamedAttributeNode("author")})
-@NamedEntityGraph(name = "book-author-genres-graph", attributeNodes = {@NamedAttributeNode("author"), @NamedAttributeNode("genres")})
+@NamedEntityGraph(name = "book-author-genres-graph", attributeNodes = {@NamedAttributeNode("author"),
+	@NamedAttributeNode("genres")})
 public class Book {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
